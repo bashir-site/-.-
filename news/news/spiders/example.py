f9d362fe04ab5d25aae65d9dfc97913f462651e2
bashir-site <59281683+bashir-site@users.noreply.github.com>
2021-05-25 16:04:01 +0300

    first_commit

---
+++ news/news/spiders/example.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import scrapy
+import json
+import math
+
+
+class RegionNews(scrapy.Spider):
+    name = 'regions'
+
+    headers = {
+        "x-requested-with": "xmlhttprequest"
+    }
+
+    def start_requests(self):
+        urls = [
+            'https://xn--90adear.xn--p1ai/news/region?perPage=20&page=1&region=77',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, headers=self.headers, callback=self.parse)
+
+    def parse(self, response):
+        raw_json = response.body
+        data = json.loads(raw_json)
+        pag = data["paginator"]
+        total_pages = math.ceil(pag["total"] / pag["perPage"])
+        # for page in range(min(5, total_pages)):
+        for page in range(total_pages):
+            yield scrapy.Request(
+                f"https://xn--90adear.xn--p1ai/r/77/news/region?perPage=20&page={page+1}&region=77",
+                callback=self.parse_news,
+                headers=self.headers,
+            )
+
+    def parse_news(self, response):
+        data = json.loads(response.body)
+        for d in data["data"]:
+            yield {
+                'Title': d['title'],
+                'Date': datetime.fromtimestamp(d['datetime']).strftime('%m-%d-%Y'),
+                'Link': f'https://xn--90adear.xn--p1ai/r/77/news/item/{d["id"]}',
+            }
